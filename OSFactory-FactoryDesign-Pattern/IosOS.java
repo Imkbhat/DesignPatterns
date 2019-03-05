74b59c191f54c42d845ee7a190cf27d10b31ae01
Kiran B S <kbs71190@gmail.com>
2019-03-05 10:30:14 +0530

    Update IosOS.java

--- OSFactory-FactoryDesign-Pattern/IosOS.java
+++ OSFactory-FactoryDesign-Pattern/IosOS.java
@@ -3,13 +3,7 @@ package com.bob.hms.admin.common;
 public class IosOS implements OS {
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-
+	public String show() {
+		return "From IosOS ";
 	}
-	
-	public void print() {
-		System.out.println("From IosOS ");
-	}
-
 }
