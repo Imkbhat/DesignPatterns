1284d8170eefc9aa99d18f3a85b3119e2a8eb982
Kiran B S <kbs71190@gmail.com>
2019-03-05 10:29:53 +0530

    Update AndroidOs.java

--- OSFactory-FactoryDesign-Pattern/AndroidOs.java
+++ OSFactory-FactoryDesign-Pattern/AndroidOs.java
@@ -3,13 +3,8 @@ package com.bob.hms.admin.common;
 public class AndroidOs implements OS {
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
+	public String show() {
+		return "From AndroidOs ";
 
 	}
-	
-	public void print() {
-		System.out.println("From AndroidOs ");
-	}
-
 }
