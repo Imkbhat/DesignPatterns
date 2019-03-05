aadcb8b496d5596e830fc9054325b764c7c6634f
Kiran B S <kbs71190@gmail.com>
2019-03-05 10:31:22 +0530

    Update ObjectFactoryRunner.java

--- OSFactory-FactoryDesign-Pattern/ObjectFactoryRunner.java
+++ OSFactory-FactoryDesign-Pattern/ObjectFactoryRunner.java
@@ -4,7 +4,7 @@ public class ObjectFactoryRunner {
 
 	public static void main(String[] args) {
 		OSObjectFactory factoryRunner = new OSObjectFactory();
-		OS os = factoryRunner.getInstance("IOS");
-		System.out.println("returning an Object of ==>" +os);
+		OS os = factoryRunner.getInstance("Windows");
+		System.out.println("returning an Object ==>" +os.show());
 	}
 }
